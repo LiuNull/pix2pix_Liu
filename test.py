f719cfe61fcfe5f8413bc61e6468502b7acfb492
LiuNull <1454291015@qq.com>
2018-11-28 09:16:14 +0800

    try to add the calculation of validation loss

--- test.py
+++ test.py
@@ -24,6 +24,8 @@ visualizer = Visualizer(opt)
 web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
 webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
 
+# try to calculate the validation loss
+
 # test
 if not opt.engine and not opt.onnx:
     model = create_model(opt)
